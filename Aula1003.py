7529113bd124b996d759b566bc77a20ceb32f26f
unknown <20251PVAI0030018@estudantes.ifpr.edu.br>
2026-03-10 09:10:36 -0300

    Inserir

---
+++ Aula1003.py
@@ -0,0 +1,23 @@
+"""alunos = [""] * 5
+for i in range (0, len(alunos)):
+    alunos[i] = input("Informe o nome do aluno")
+
+cont = 1
+for alu in range (0, len(alunos)):  
+    print("Aluno ",cont, alunos[alu])
+    cont = cont +1 """
+
+#ex 32
+
+nome = input("Digite o nome ")
+idade = int(input("Digite a idade"))
+sexo = bool(input("Digite o sexo"))
+
+if (sexo == False and idade >= 30):
+    print("Você foi aceita!")
+elif(sexo == True and idade > 25 ):
+    print("Você foi aceito!")  
+elif((sexo == True or sexo == False)and (idade > 10 and idade < 15 )):
+    print("Você foi aceito!")
+else:
+    print("Você não foi aceito.")
---
+++ github.py
@@ -0,0 +1,23 @@
+import os
+
+print("Configurando email...")
+comandoEmail = "git config user.email '20251PVAI0030018@estudantes.ifpr.edu.br'"
+os.system(comandoEmail)
+
+print("Adicionando modificações...")
+comandoAdd = "git add *"
+os.system(comandoAdd)
+
+mensagem = input("Mensagem do commit: ")
+
+while(len(mensagem) < 5 ):
+    print("Mensagem muito pequena, detalhe mais.🥴")
+    mensagem = input("Mensagem do commit: ")
+
+print("Registrando Alterações...")
+comandoCommit = f"git commit -m{mensagem}"
+os.system(comandoCommit)
+
+print("Enviando projeto ao github")
+comandoPush = "git push"
+os.system(comandoPush)
